3fb9d11157f22f8c07de757222b67d89e6832f12
Lindsey Martin <martinlindsey87@yahoo.com>
2022-05-12 21:50:57 -0700

    added save to local storage and display function

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -1,9 +1,60 @@
 var APIKey = "021b34237bbcc96949e3a99359d6328e";
-var city;
+// var city;
 
 var today = moment().format('L');
-var searchHistoryList = [];
 
+
+
+
+var cityInputEL = document.querySelector('#enterCity');
+
+
+var searchHistoryList =
+    JSON.parse(window.localStorage.getItem("searchHistoryList")) || [];
+
+//save to local storage function
+var saveCity = function () {
+    //get value from input box
+    var CityInput = cityInputEL.value.trim();
+    console.log('saveCity function');
+    if (cityInputEL !== "") {
+        var newCity = {
+            city: CityInput,
+        };
+
+        searchHistoryList.push(newCity);
+        window.localStorage.setItem("newCity", JSON.stringify(searchHistoryList));
+    }
+    displayEnteredCity();
+};
+
+//display previous cities 
+var displayEnteredCity = function () {
+    console.log(displayEnteredCity);
+
+    // display on page
+    var olEl = document.getElementById("searchHistory");
+    olEl.textContent = "";
+
+    searchHistoryList.forEach(function ({
+
+        city
+    }) {
+        // create li tag for each item
+        var liEl = document.createElement("li");
+        // liEl.textContent = drink + " &  zipCode";
+        liEl.textContent = city;
+        olEl.appendChild(liEl);
+
+
+    });
+
+};
+
+
+
+//current city fetch
 var currentCondition = function (city) {
+
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-}
+};
