fe3f9a74adc59898b371778796bc9ddb6a024360
Lindsey Martin <martinlindsey87@yahoo.com>
2022-05-13 16:36:22 -0700

    updated fetch call and cleaned up code

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -1,59 +1,106 @@
 var APIKey = "021b34237bbcc96949e3a99359d6328e";
 
-
 var today = moment().format('L');
 var cityInputEL = document.querySelector('#enterCity');
-
+var saveCityButton = document.querySelector('#searchBtn');
 
 var searchHistoryList =
     JSON.parse(window.localStorage.getItem("searchHistoryList")) || [];
 
-//save to local storage function
-var saveCity = function () {
+var saveCity = function(city) {
     //get value from input box
-    var CityInput = cityInputEL.value.trim();
     console.log('saveCity function');
-    if (cityInputEL !== "") {
-        var newCity = {
-            city: CityInput,
-        };
+    var newCity = {
+        city,
+    };
 
-        searchHistoryList.push(newCity);
-        window.localStorage.setItem("newCity", JSON.stringify(searchHistoryList));
-    }
-    displayEnteredCity();
+    searchHistoryList.push(newCity);
+    window.localStorage.setItem("newCity", JSON.stringify(searchHistoryList));
 };
 
-//display previous cities 
-var displayEnteredCity = function () {
-    console.log(displayEnteredCity);
+//display search history of cities 
+var displaySearchHistory = function () {
+    console.log(displaySearchHistory);
 
     // display on page
     var olEl = document.getElementById("searchHistory");
     olEl.textContent = "";
 
-    searchHistoryList.forEach(function ({
-
-        city
-    }) {
+    searchHistoryList.forEach(function ({ city }) {
         // create li tag for each item
         var liEl = document.createElement("li");
-        // liEl.textContent = drink + " &  zipCode";
         liEl.textContent = city;
         olEl.appendChild(liEl);
 
 
     });
-    currentCondition();
 };
 
+//display search history of cities 
+var displayCurrentWeather = function (currentWeather) {
+    console.log(currentWeather)
+};
 
+//display search history of cities 
+var displayForecast = function (dailyWeather) {
+    console.log(dailyWeather)
+};
+
+//display search history of cities 
+var displaySearchHistory = function () {
+    console.log(displaySearchHistory);
+
+    // display on page
+    var olEl = document.getElementById("searchHistory");
+    olEl.textContent = "";
+
+    searchHistoryList.forEach(function ({ city }) {
+        // create li tag for each item
+        var liEl = document.createElement("li");
+        liEl.textContent = city;
+        olEl.appendChild(liEl);
+
+
+    });
+};
 
 //current city fetch
-var currentCondition = function (city) {
+var fetchCurrentCondition = function (city) {
     console.log(city);
-    var CityInput = cityInputEL.value.trim();
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-    fetch(queryURL);
+    var currentWeatherQueryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+    fetch(currentWeatherQueryURL)
+        .then(function(res) { 
+            return res.json()
+        })
+        .then(function(data) {
+            return data.coord;
+        })
+        .then(function(coord) {
+            console.log(coord)
+            var oneCallQueryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&appid=${APIKey}`;
+            return fetch(oneCallQueryURL);
+        })
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(weather) {
+            displayCurrentWeather(weather.current);
+            displayForecast(weather.daily.slice(1,6));
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
 
 };
+
+//save to local storage function
+saveCityButton.addEventListener("click", function () {
+    //get value from input box
+    var cityInput = cityInputEL.value.trim();
+    if (cityInput === "") {
+        return;
+    }
+    saveCity(cityInput);
+    displaySearchHistory();
+    fetchCurrentCondition(cityInput);
+});
